1bea28b2596edd6110caa5298d0f49e3655ef5eb
tanisha0605 <tanishakanal@gmail.com>
2024-04-28 11:09:01 +0530

    Navigation added for Add Button

--- client/src/components/AddButton.jsx
+++ client/src/components/AddButton.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from 'react';
 import Button from '@mui/material/Button';
-
+import { useNavigate } from 'react-router-dom';
 import AddIcon from '@mui/icons-material/Add';
-function AddButton({ModelName}) {
+
+function AddButton({ ModelName }) {
+  const navigate = useNavigate();
+
+  const handleAddButtonClick = () => {
+    navigate(`/${ModelName.toLowerCase()}/add-${ModelName.toLowerCase()}`);
+  };
+
   return (
     <div className="flex justify-end mr-10 mt-5">
-        <Button variant="contained" endIcon={<AddIcon />}>
-            Add {ModelName}
-        </Button>
+      <Button variant="contained" endIcon={<AddIcon />} onClick={handleAddButtonClick}>
+        Add {ModelName}
+      </Button>
     </div>
-  )
+  );
 }
 
-export default AddButton
+export default AddButton;
